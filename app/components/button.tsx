a26f17b39c0bec469dd6da87673e945ce1b4f759
shresthasusan <shresthasusan44@gmail.com>
2024-12-28 22:55:24 +0545

    add Button component for navigation with customizable link and styles

---
+++ app/components/button.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+
+interface ButtonProps {
+  link: string;
+  className?: string;
+}
+
+const Button = ({ link, className }: ButtonProps) => {
+  return (
+    <a
+      href={link}
+      className={`bg-primary text-white px-4 py-3 rounded-xl font-semibold md:text-lg  text-sm ${className}`}
+    >
+      Register Now{" "}
+    </a>
+  );
+};
+
+export default Button;
