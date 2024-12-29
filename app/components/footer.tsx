15caf2fdcf9b9fba85af68f688c10e25905d19a5
shresthasusan <shresthasusan44@gmail.com>
2024-12-29 22:20:36 +0545

    refactor Footer component to remove unused useRef import

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
 import { Facebook01Icon, InstagramIcon, Linkedin01Icon } from "hugeicons-react";
 
