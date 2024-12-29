2beef6e8fd5461539193e8e1ec17b4f94c61d8b3
shresthasusan <shresthasusan44@gmail.com>
2024-12-29 22:18:17 +0545

    refactor Navbar component to implement Intersection Observer for active section tracking and add smooth scrolling behavior

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,22 +1,52 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  const handleScroll = () => {
-    const position = window.scrollY;
-    setScrollPosition(position);
-  };
+  const [activeSection, setActiveSection] = useState("home");
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    const sections = document.querySelectorAll("section[id]");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    sections.forEach((section) => observer.observe(section));
+
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      sections.forEach((section) => observer.unobserve(section));
     };
-  }, []);
+  }, [setActiveSection]);
+  // Smooth Scroll to Section with offset for floating footer
+  const scrollToSection = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    id: string
+  ) => {
+    e.preventDefault();
+    const target = document.querySelector(`#${id}`);
+    const footer = document.querySelector("footer");
+    const footerHeight = footer ? footer.offsetHeight + 500 : 0; // Get footer height
+    if (target) {
+      window.scrollTo({
+        top: (target as HTMLElement).offsetTop + footerHeight, // Subtract footer height from scroll position
+        behavior: "smooth",
+      });
+      setTimeout(() => {
+        window.scrollBy({
+          top: 500, // Scroll additional 100 pixels
+          behavior: "smooth",
+        });
+      }, 1000);
+    }
+  };
 
   return (
     <section className="nav-section max-w-screen-xl md:text-lg text-base p-2 mx-auto">
@@ -29,38 +59,44 @@ const Navbar = () => {
             alt={"logo"}
           />
         </div>
-        <ul className="nav-links flex gap-4">
+        <ul className="nav-links flex gap-6">
           <li
-            className={`hover:text-dark transition-all duration-400 ${
-              scrollPosition < 100 ? "text-primary" : "text-muted"
+            className={` transition-all duration-300 active:text-primary ${
+              activeSection === "home"
+                ? "text-primary"
+                : "text-muted hover:text-dark"
             }`}
           >
             <a href="#home">Home</a>
           </li>
           <li
-            className={`hover:text-dark transition-all duration-400 ${
-              scrollPosition >= 700 && scrollPosition < 2000
+            className={` transition-all duration-300 ${
+              activeSection === "about"
                 ? "text-primary"
-                : "text-muted"
+                : "text-muted hover:text-dark"
             }`}
           >
             <a href="#about">About</a>
           </li>
           <li
-            className={`hover:text-dark transition-all duration-400 ${
-              scrollPosition >= 2000 && scrollPosition < 3000
+            className={` transition-all duration-300 ${
+              activeSection === "faq"
                 ? "text-primary"
-                : "text-muted"
+                : "text-muted hover:text-dark"
             }`}
           >
             <a href="#faq">FAQ</a>
           </li>
           <li
-            className={`hover:text-dark transition-all duration-400 ${
-              scrollPosition >= 3000 ? "text-primary" : "text-muted"
+            className={` transition-all duration-300 ${
+              activeSection === "contact"
+                ? "text-primary"
+                : "text-muted hover:text-dark"
             }`}
           >
-            <a href="#contact">Contact</a>
+            <a href="#contact" onClick={(e) => scrollToSection(e, "contact")}>
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
