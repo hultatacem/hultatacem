523eb3da2bf99e3a2bf6666a2285e323e784ba82
shresthasusan <shresthasusan44@gmail.com>
2024-12-29 22:20:45 +0545

    refactor HeroSection component for improved layout and responsive design

--- app/components/main-section/heroSection.tsx
+++ app/components/main-section/heroSection.tsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section
-      className="hero-section relative  md:pt-44 pt-32 px-2 -top-[80px] overflow-hidden"
+      className="hero-section relative flex items-center justify-center min-h-screen overflow-hidden p-20"
       id="home"
     >
-      <section className="hero-content flex flex-col justify-evenly lg:gap-16 md:gap-16 gap-8">
+      <section className="hero-content flex flex-col justify-evenly gap-8 md:gap-16">
         <section className="text-section flex flex-col items-center justify-center text-center text-white">
-          <h2 className="md:text-4xl text-1xl font-bold ">
+          <h2 className="font-bold text-[clamp(1.5rem,3vw,3rem)]">
             Hult Acem presents
           </h2>
-          <h1 className="md:text-8xl text-4xl font-bold text-primary">
+          <h1 className="text-primary font-bold text-[clamp(2rem,5vw,6rem)]">
             Hult Prize onCampus
           </h1>
-          <p className="md:text-2xl text-lg mt-4 font-medium">
+          <p className="mt-4 font-medium text-[clamp(1rem,2.5vw,1.8rem)]">
             Empowering the Next Generation of Social Entrepreneurs
           </p>
           <br />
@@ -27,31 +27,28 @@ const HeroSection = () => {
           />
         </section>
         <section className="logos-section flex justify-center flex-wrap items-center gap-10">
-          <span className="md:w-[300px] ">
+          <span className="w-[clamp(100px,30vw,300px)]">
             <Image
               src={"/hult-main-logo.png"}
-              alt={""}
-              sizes="(max-width: 568px) 100vw, (max-width: 500px) 50vw, 33vw"
-              width={300}
-              height={300}
+              alt="Hult Logo"
+              width={400}
+              height={400}
             />
           </span>
-          <span className="md:w-[120px] ">
+          <span className="w-[clamp(100px,20vw,130px)]">
             <Image
               src={"/hult-acem.png"}
-              alt={""}
-              sizes="(max-width: 568px) 100vw, (max-width: 500px) 50vw, 33vw"
-              width={120}
-              height={120}
+              alt="Hult Acem"
+              width={150}
+              height={150}
             />
           </span>
-          <span className="md:w-[300px] ">
+          <span className="w-[clamp(100px,30vw,300px)]">
             <Image
               src={"/onCampus.png"}
-              alt={""}
-              sizes="(max-width: 568px) 100vw, (max-width: 500px) 50vw, 33vw"
-              width={300}
-              height={300}
+              alt="On Campus"
+              width={400}
+              height={400}
             />
           </span>
         </section>
